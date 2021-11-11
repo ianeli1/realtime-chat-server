08230e4567f638d11c4bccad6a3ba81e6897c134
Ian <ianelicha@gmail.com>
2021-11-10 20:17:56 -0600

    Add new controllers and services to ChatModule

--- src/modules/chat.module.ts
+++ src/modules/chat.module.ts
@@ -1,17 +1,26 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MessageController } from "../controllers/message.controller";
 import { TeamsController } from "../controllers/team.controller";
+import { TopicController } from "../controllers/topic.controller";
 import { UserController } from "../controllers/user.controller";
 import { Message } from "../entity/Message.entity";
 import { Team } from "../entity/Team.entity";
 import { Topic } from "../entity/Topic.entity";
 import { User } from "../entity/User.entity";
+import { MessageService } from "../services/message.service";
 import { TeamService } from "../services/team.service";
+import { TopicService } from "../services/topic.service";
 import { UserService } from "../services/user.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Team, Topic, Message])],
-  providers: [UserService, TeamService],
-  controllers: [UserController, TeamsController],
+  providers: [UserService, TeamService, TopicService, MessageService],
+  controllers: [
+    UserController,
+    TeamsController,
+    TopicController,
+    MessageController,
+  ],
 })
 export class ChatModule {}
