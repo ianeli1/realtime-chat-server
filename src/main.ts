90a0dfb0c63d970890220ef674e80435729fdbfe
Ian <ianelicha@gmail.com>
2021-11-10 20:18:07 -0600

    Install Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import "reflect-metadata";
-import { Connection } from "./connection";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle("realtime-chat-server").build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("", app, document);
+
   await app.listen(3000);
 }
 
